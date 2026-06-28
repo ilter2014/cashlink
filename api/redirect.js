e4c7939ef15e7c28a362f7b1f787b324b08244fa
ilter2014 <ilter9047@gmail.com>
2026-06-29 01:50:50 +0300

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -6,7 +6,7 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // TIKLAMA BAŞINA VERİLECEK FİYAT (İleride burayı değiştirebilirsin)
-const CLICK_PRICE = 100.00; 
+const CLICK_PRICE = 110.00; 
 
 export default async function handler(req, res) {
     const { code } = req.query;
